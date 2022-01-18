ed330898eca47546e5938075a18d9953330ac573
k3t4 <k3tchll@yandex.ru>
2022-01-18 21:51:05 +0300

    first working version

---
+++ test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Comp = () => {
+  return (
+    foo
+      <Component>
+        <ComponentTwo>
+          <div>sdfgsdfg</div>
+        </ComponentTwo>
+      </Component>
+    bar
+  )
+}
+
+foo
+  content
+bar
